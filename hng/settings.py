f4b075d4d585701eaac49db6f49917f671f0e71f
mjoghenemaega <mjoghenemaega@gmail.com>
2025-02-04 15:03:24 +0100

    added allowed host

--- hng/settings.py
+++ hng/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@%u+jpzb9_77!^@vwtq^1&&ho2k+573zj0owyj*da63!_vmt0+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'hng12-one.vercel.app','127.0.0.1']
 
 
 # Application definition
